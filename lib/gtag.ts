13ccbe38011ca50efc014efe1a7b50fc789c250c
miffto <miffto@gmail.com>
2021-09-16 19:04:38 +0900

    hogeeeeeee

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -4,7 +4,10 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || '';
 // PV 測定
 export const pageview = (url: string): void => {
   // GA_TRACKING_ID が設定されていない場合は、処理終了
-  if (!GA_TRACKING_ID) return;
+  if (!GA_TRACKING_ID){
+    console.log('hogeeeeeeeeee')
+    return;
+  };
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
   });
