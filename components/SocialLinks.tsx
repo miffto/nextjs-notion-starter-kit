c35b7ef9329515211a082043ed861e60bb468519
miffto <miffto@gmail.com>
2021-09-27 13:33:37 +0900

    notion.css戻した

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -6,7 +6,7 @@ import * as config from 'lib/config'
 import styles from './styles.module.css'
 
 interface Props {
-  
+
 }
 
 export const SocialLinks = (props: Props) => {
@@ -24,54 +24,6 @@ export const SocialLinks = (props: Props) => {
           </a>
         )}
 
-         {config.twitch && (
-          <a
-            className={styles.twitter}
-            href={`https://www.twitch.tv/${config.twitch}`}
-            title={`Twitch @${config.twitch}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaTwitch />
-          </a>
-        )}
-
-        {config.letterboxd && (
-          <a
-            className={styles.twitter}
-            href={`https://letterboxd.com/${config.letterboxd}`}
-            title={`Letterboxd @${config.letterboxd}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <SiLetterboxd />
-          </a>
-        )}
-
-{config.instagram && (
-          <a
-            className={styles.twitter}
-            href={`https://www.instagram.com/${config.instagram}`}
-            title={`Instagram @${config.instagram}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaInstagram />
-          </a>
-        )}
-
-{config.goodreads && (
-          <a
-            className={styles.twitter}
-            href={`https://www.goodreads.com/user/show/${config.goodreads}`}
-            title={`Goodreads @${config.goodreads}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaGoodreads />
-          </a>
-        )}
-
         {config.github && (
           <a
             className={styles.github}
@@ -84,18 +36,6 @@ export const SocialLinks = (props: Props) => {
           </a>
         )}
 
-        {config.linkedin && (
-          <a
-            className={styles.linkedin}
-            href={`https://www.linkedin.com/in/${config.linkedin}`}
-            title={`LinkedIn ${config.author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaLinkedin />
-          </a>
-        )}
-      
     </>
   )
 }
