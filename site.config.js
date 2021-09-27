3805e81ebcf4f9a7854e188c976d27975e380fe7
miffto <miffto@gmail.com>
2021-09-27 17:04:41 +0900

    pageUrlOverrides できないんだが

--- site.config.js
+++ site.config.js
@@ -43,9 +43,9 @@ module.exports = {
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
+  pageUrlOverrides: {
+     'Blog': '2b3f212117dd4caf9ccb9323d3bed4c4'
+   },
+
   pageUrlOverrides: null
 }
